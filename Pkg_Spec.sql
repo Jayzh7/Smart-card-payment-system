cc4850df2c2b0ed4342c72dfa0a698048b41da6c
Unknown <zienjih@gmail.com>
2018-05-26 14:25:52 +1000

    Fix to_date bug and implement daily report procedure
    Still need to improve the format

--- Pkg_Spec.sql
+++ Pkg_Spec.sql
@@ -2,7 +2,7 @@ CREATE PACKAGE Pkg_FSS_Settlement
 AS
     PROCEDURE DailySettlement;
     
-    --PROCEDURE DailyBankingSummary(d in DATE);
+    PROCEDURE DailyBankingSummary;
 
     --PROCEDURE FraudReport;
 END Pkg_FSS_Settlement;
